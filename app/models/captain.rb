63a6b827de5973e4c0e9bd02fe6c61bf2631ee3f
Dee Race <deerace12@gmail.com>
2018-06-27 20:41:45 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -3,21 +3,21 @@ class Captain < ActiveRecord::Base
 
   def self.catamaran_operators
     includes(boats: :classifications).where(classifications: {name: 'Catamaran'}).uniq
-  end 
+  end
 
-  def self.sailors 
+  def self.sailors
     includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
-  end 
+  end
 
   def self.motorboat_operators
     includes(boats: :classifications).where(classifications: {name: 'Motorboat'}).uniq
-  end 
+  end
 
   def self.talented_seafarers
     where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
-  end 
+  end
 
-  def self.non_sailors 
+  def self.non_sailors
     where.not("id IN (?)", self.sailors.pluch(:id))
   end 
 end
